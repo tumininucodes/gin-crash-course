94a87c1e3cb6a8e98d53de7bff67716ae5c7e84d
Oluwatumininu Ojo <oluwatumininuojo25@gmail.com>
2023-01-24 15:34:06 +0100

    development

--- server.go
+++ server.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"io"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/tumininucodes/gin-crash-course/controller"
 	"github.com/tumininucodes/gin-crash-course/entity/service"
+	"github.com/tumininucodes/gin-crash-course/middlewares"
 )
 
 var(
@@ -11,8 +15,17 @@ var(
 	videoController controller.VideoController = controller.New(videoService)
 )
 
+func setupLogOuput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
 func main() {
-	server := gin.Default()
+
+	setupLogOuput()
+	server := gin.New()
+
+	server.Use(gin.Recovery(), middlewares.Logger())
 
 	server.GET("/videos", func(ctx *gin.Context) {
 		ctx.JSON(200, videoController.FindAll())
